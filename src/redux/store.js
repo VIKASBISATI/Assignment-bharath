2e3f7dc8c42708ea6b69d6341ee82067458d2d69
Vikas bisati <bsaivenkatavikas@gmail.com>
2020-06-19 22:45:24 +0530

    removed chrome dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,8 +14,6 @@ const store = createStore(
   reducers,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+    applyMiddleware(...middleware)  )
 );
 export default store;
